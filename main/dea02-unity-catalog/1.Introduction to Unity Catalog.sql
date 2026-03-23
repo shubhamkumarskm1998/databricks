efbdce191c5f57512b95a2384e52aebfdd545507
Shubham <shubhammainwal@gmail.com>
2026-03-23 22:59:19 +0530

    removed unwanted files

--- main/dea02-unity-catalog/1.Introduction to Unity Catalog.sql
+++
@@ -1,7 +0,0 @@
--- Databricks notebook source
--- MAGIC %md
--- MAGIC ### Check whether the workspace is attached to a Unity Catalog Metastore
-
--- COMMAND ----------
-
-SELECT current_metastore();
